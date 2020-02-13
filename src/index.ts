4ab10b360bb05aa1854d69abcca3294b94ed460a
Yz_4230 <yuzupon1480@gmail.com>
2020-02-13 14:38:30 +0000

    リアルタイムデバッグを可能にした

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-import { Client } from "pg";
-
-const client = new Client({
-    user: "yz",
-    password: "805880",
-    database: "yzdb",
-    host: "database",
-    port: 5432
-});
-client.connect();
-client.query("SELECT NOW()", (err, res) => {
-    console.log(res);
-    client.end();
-});
-
-console.log("hello pg");
+import * as express from "express";
+const app = express();
+console.log("hello world!");
